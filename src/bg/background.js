0c5aedf3ced2c62914414b455125d2aead2176f5
Guzzardo <guzzardo.joe@gmail.com>
2016-09-10 16:43:42 -0400

    Clicking works!

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,5 +1,11 @@
-chrome.browserExtension.onClicked.addListener(function(tab)) {
-	chrome.tabs.executeScript(null, {
-		file: "popup.js"
-	})
-}
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    file: 'js/jquery/jquery.min.js',
+  });
+  chrome.tabs.executeScript({
+    code: "$('a').contents().unwrap();"
+  });
+});
