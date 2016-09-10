d5a0ffa6be7c65c27f985d727d1cfe64e0b7137a
Guzzardo <guzzardo.joe@gmail.com>
2016-09-10 16:48:59 -0400

    remove sample logging

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,7 +1,6 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
   chrome.tabs.executeScript({
     file: 'js/jquery/jquery.min.js',
   });
