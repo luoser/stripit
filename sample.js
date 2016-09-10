59d7a75c72a01504086ef2edd8644789f5411e7a
Lisa Luo <lluo92@gmail.com>
2016-09-10 15:34:32 -0400

    remove underscore

--- sample.js
+++ sample.js
@@ -20,7 +20,6 @@ for (var i = 0; i < contexts.length; i++) {
   console.log("'" + context + "' item:" + id);
 }
 
-
 // Create a parent item and two children.
 var parent = chrome.contextMenus.create({"title": "Test parent item"});
 var child1 = chrome.contextMenus.create(
@@ -28,19 +27,6 @@ var child1 = chrome.contextMenus.create(
 var child2 = chrome.contextMenus.create(
   {"title": "Child 2", "parentId": parent, "onclick": genericOnClick});
 console.log("parent:" + parent + " child1:" + child1 + " child2:" + child2);
-
-
-// Create some radio items.
-function radioOnClick(info, tab) {
-  console.log("radio item " + info.menuItemId +
-              " was clicked (previous checked state was "  +
-              info.wasChecked + ")");
-}
-var radio1 = chrome.contextMenus.create({"title": "Radio 1", "type": "radio",
-                                         "onclick":radioOnClick});
-var radio2 = chrome.contextMenus.create({"title": "Radio 2", "type": "radio",
-                                         "onclick":radioOnClick});
-console.log("radio1:" + radio1 + " radio2:" + radio2);
 
 
 // Create some checkbox items.
